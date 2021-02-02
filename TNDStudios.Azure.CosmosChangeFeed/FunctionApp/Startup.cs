7b172e7eb0de0ebfefddd527ea7455063b0cfb57
TNDStudios <info@thenakeddeveloper.com>
2021-02-02 23:38:02 +0000

    Added DI and function startup

--- TNDStudios.Azure.CosmosChangeFeed/FunctionApp/Startup.cs
+++ TNDStudios.Azure.CosmosChangeFeed/FunctionApp/Startup.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FunctionApp.Startup))]
 
 namespace FunctionApp
 {
-    public class Startup
+    public class Startup : FunctionsStartup
     {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+
+        }
     }
 }
